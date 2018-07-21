b9e5cc25c766fb5ed5fc6651be8ab2b2154939ec
Armin Sander <armin@replicator.org>
2018-07-21 17:15:44 +0200

    port to NetStandard

--- Collections.Fluent/Properties/AssemblyInfo.cs
+++ Collections.Fluent/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Collections.Fluent")]
-[assembly: AssemblyCopyright("Copyright © Armin Sander 2015")]
+[assembly: AssemblyCopyright("Copyright © Armin Sander 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
